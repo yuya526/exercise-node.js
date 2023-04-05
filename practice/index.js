f46b974cb78007a251653e29475e7ba52c9a3497
yuya kito <129923011+yuya526@users.noreply.github.com>
2023-04-05 18:10:13 +0900

    initial commit
    練習です

---
+++ practice/index.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+
+console.log('math.add(1, 2) : ', math.add(1, 2));
+console.log('math.sub(3, 4) : ', math.sub(3, 4));
+console.log('math.mul(5, 6) : ', math.mul(5, 6));
+console.log('math.div(7, 8) : ', math.div(7, 8));
---
+++ practice/main.js
@@ -0,0 +1,19 @@
+const math = {
+  add: (x, y) => {
+    return x + y;
+  },
+
+  sub: (x, y) => {
+    return x - y;
+  },
+
+  mul: (x, y) => {
+    return x * y;
+  },
+
+  div: (x, y) => {
+    return x / y;
+  }
+};
+
+module.exports = math;
